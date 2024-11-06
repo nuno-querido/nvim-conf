bdc5e84edbb07b30e8422cca0710973caa3d4d80
nuno-querido <nuno.querido.dev@gmail.com>
2024-11-06 23:50:20 +0000

    feat(packages): improved trouble and added todo highlights

--- lua/querido/lazy/trouble.lua
+++ lua/querido/lazy/trouble.lua
@@ -1,3 +1,4 @@
+--[[
 return {
     {
         "folke/trouble.nvim",
@@ -19,5 +20,24 @@ return {
             end)
 
         end
+    },
+}
+--]]
+
+return {
+    "folke/trouble.nvim",
+    opts = {},   -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle focus=false win.position=right<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>tl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
     },
 }
