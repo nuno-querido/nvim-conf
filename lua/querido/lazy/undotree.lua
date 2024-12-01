8049590906f0cd2201a4326b6f652e2e45b2e9d2
nuno-querido <nuno.querido.dev@gmail.com>
2024-12-01 22:15:56 +0000

    feat(undotree): now hiding nvim tree when opening undotree
    and vice versa

--- lua/querido/lazy/undotree.lua
+++ lua/querido/lazy/undotree.lua
@@ -1,7 +1,25 @@
+local is_undo_open = false;
+
 return {
     "mbbill/undotree",
 
     config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+        -- for some off reason nvim tree has a 25% width but it's still wider than this...
+        vim.g.undotree_SplitWidth = "40%"
+        vim.keymap.set(
+            "n",
+            "<leader>u",
+            function()
+                local tree = require("nvim-tree.api").tree
+
+                -- too lazy to find the correct way to invert a boolean value in lua :)
+                is_undo_open = is_undo_open == false;
+                vim.cmd.UndotreeToggle()
+
+                if tree.is_visible() == is_undo_open then
+                    tree.toggle({ focus = false })
+                end
+            end
+        )
     end
 }
