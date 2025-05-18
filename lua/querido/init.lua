a58d700fe35d632944d8341c743704e6e93da5d3
nuno-querido <nuno.querido.dev@gmail.com>
2025-05-18 20:33:41 +0100

    refactor(mason): updated mason config to force old version
    this was breaking eslint server

--- lua/querido/init.lua
+++ lua/querido/init.lua
@@ -37,7 +37,14 @@ autocmd('LspAttach', {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set(
+            "n",
+            "K",
+            function()
+                vim.lsp.buf.hover({ border = "rounded" })
+            end,
+            opts
+        )
         vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
         vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
         vim.keymap.set(
